67a9fa166ba547713551e5788c50e5d8a50089ba
Rafael Dantas <srdantas.rafael@gmail.com>
2019-11-01 20:13:00 -0300

    Primeiro versionamento

---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Maculino = 1,
+        Feminino = 2 
+    }
+}
